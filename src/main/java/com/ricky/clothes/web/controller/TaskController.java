a009ad3516cacbde905e6843bbe4154c688e8f3a
CaiQing <cqricky@163.com>
2015-11-18 23:10:57 +0800

    add test

--- src/main/java/com/ricky/clothes/web/controller/TaskController.java
+++ src/main/java/com/ricky/clothes/web/controller/TaskController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +22,7 @@ public class TaskController extends BaseAppController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Task> getAllTaks() {
         return taskService.findAll();
+//        return new ArrayList<Task>();
     }
 
 }
